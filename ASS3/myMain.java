e76ef52d0220ddfd119755b69dcbc24b5f054181
Cassim <111745304+catsums@users.noreply.github.com>
2023-05-30 09:18:28 +0200

    Im suffering wtf

--- ASS3/myMain.java
+++ ASS3/myMain.java
@@ -19,11 +19,11 @@ public class myMain{
         My.cout("m0");
         My.cout("---------------");
 
-		int numOfInputs = 3;
-		int numOfInst = 2;
+		int inputSize = 3;
+		int instSize = 1;
 
-		double[][] w = initMatrix(numOfInputs, numOfInst);
-		double[] b = new double[numOfInst];
+		double[][] w = initMatrix(inputSize, instSize);
+		double[] b = new double[instSize];
 		for(int i=0;i<b.length;i++) b[i] = 0;
 		
 		My.cout(printMatrix(w));
@@ -31,46 +31,79 @@ public class myMain{
 		double[][] p = new double[][]{
 			{1,-1,-1},
 			{1,1,-1},
+			{1,-1,1},
+			{0,0,0},
 		};
-		double[] t = new double[]{
-			-1,
-			1
+		double[][] t = new double[][]{
+			{-1, 1},
+			{1, -1},
+			{1, 1},
+			{-1, -1},
 		};
 		
 		double lRate = 1;
+
+		boolean conv = false;
+		int epochCount = 0;
+		int epochLimit = 100;
+		while(!conv){
+			conv = true;
+			for(int j=0; j<instSize; j++){
+				int numOfInputs = p.length;
+				for(int i=0; i<numOfInputs; i++){
+					My.cout("i:"+i+" j:"+j);
+					
+					double n = weightedSum(j, b[j], p[i], w);
+					double fn = MCPitts(n, 0, true);
+					
+					My.cout("n:"+n);
+					My.cout("f(n):"+fn);
 		
-		int j = 0;
-		for(int i=0; i<numOfInst; i++){
-			My.cout("i:"+i);
-			
-			double n = calcN(j, b[j], p[i], w);
-			double fn = MCPitts(n, 0, true);
-			
-			My.cout("n:"+n);
-			My.cout("f(n):"+fn);
-
-			if(fn != t[i]){
-				setMatrixCol(w, j, 
-					updateWeight(
-						getMatrixCol(w, j)
-					, lRate, t[i],fn, p[i])
-				);
-
-				b[j] = b[j] + lRate * (t[i] - fn);
+					if(fn != t[i][j]){
+						conv = false;
+						setMatrixCol(w, j, 
+							updateWeight(
+								getMatrixCol(w, j)
+							, lRate, t[i][j],fn, p[i])
+						);
+		
+						b[j] = b[j] + lRate * (t[i][j] - fn);
+					}
+		
+					My.cout("w:\n"+printMatrix(w));
+					My.cout("b:"+b[j]);
+					My.cout("------------");
+				}
 			}
-
-			My.cout("w:\n"+printMatrix(w));
-			My.cout("b:"+b[j]);
-			My.cout("------------");
+			epochCount++;
 		}
+		
 
 		
-		My.cout(printMatrix(w));
-		My.cout(b[j]);
+		My.cout("w:\n"+printMatrix(w));
+		My.cout("b:\n"+printVector(b));
+		// My.cout("t:\n"+printMatrix(t));
+		// My.cout("p:\n"+printMatrix(p));
+
+		My.cout("-----------");
+
+		double[] input = new double[]{1,1,1};
+		double[] output = new double[instSize];
+		for(int j=0;j<instSize;j++){	
+			double n = weightedSum(j, b[j], input, w);
+			double fn = MCPitts(n, 0, true);
+	
+			My.cout("f(n): "+fn);
+			output[j] = fn;
+		}
+		
+
+		My.cout("in: "+printVector(input));
+		My.cout("out: "+printVector(output));
 
     }
 
-	public static double calcN(int c, double b, double[] p, double[][] w){
+	public static double weightedSum(int c, double b, double[] p, double[][] w){
 		/*
 		 * n = SUM(w[i][j] * p[i] + b)
 		 * where m = number of inputs
@@ -87,6 +120,46 @@ public class myMain{
         }
         return n;
 
+	}
+
+	public static double errorInfoOutput(double t, double fn2, double dirFn2){
+		//Qk = (tk - f(n2k)) * f'(n2k)
+		//where k = 1 to m
+		return (t - fn2) * dirFn2;
+	}
+	public static double errorInfoHidden(double[][] w, int index, double tk, double fn2k, double dirFn2k, double dirFn1i){
+		// Qi = Qni * f'(n1i)
+		// = SUM( ( (tk - f(n2k) ) * f'(n2k)) * wik ) * f'(n1i)
+		//where i = 1 to j
+		//where k = 1 to m
+		return (sumOfDeltaInputs(w, index, tk, fn2k, dirFn2k) * dirFn1i);
+	}
+	
+	public static double weightCorrectionOutput(double lRate, double tk, double fn2k, double dirFn2k, double fn1i){
+		// DELTA wik = lRate * Qk * f'(n1i)
+		// = lRate * ((tk - f(n2k)) * f'(n2k)) * f'(n1i)
+		//where k = 1 to m
+		//where i = 1 to j
+		return lRate * errorInfoOutput(tk, fn2k, dirFn2k) * fn1i;
+	}
+
+	public static double biasCorrectionOutput(double lRate, double tk, double fn2k, double dirFn2k){
+		// DELTA w0k = lRate * Qk
+		// = lRate * ((tk - f(n2k)) * f'(n2k))
+		//where k = 1 to m
+		return lRate * errorInfoOutput(tk, fn2k, dirFn2k);
+	}
+
+	public static double sumOfDeltaInputs(double[][] w, int index, double tk, double fn2k, double dirFn2k){
+		// Qni = SUM(Qk * wik)
+		// = SUM( ( (tk - f(n2k) ) * f'(n2k)) * wik )
+		//where k = 1 to m
+		//where i = i to j
+		double n = 0;
+		for(int k=0; k<w[index].length; k++){
+			n += errorInfoOutput(tk, fn2k, dirFn2k) * w[index][k];
+		}
+		return n;
 	}
 
 	public static double[] updateWeight(double[] wi, double lRate, double t, double fn, double[] p){
