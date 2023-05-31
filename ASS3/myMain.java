de1b77f23ea14180214b950123b589f2e312d9de
Cassim <111745304+catsums@users.noreply.github.com>
2023-05-31 18:06:39 +0200

    Autocommit at Wed May 31 18:06:39 SAST 2023

--- ASS3/myMain.java
+++ ASS3/myMain.java
@@ -114,14 +114,14 @@ public class myMain{
 		int instSize = 2; //J
 		int outputSize = 2; //M
 
-		double[][] v = initMatrix(inputSize+1, instSize, -0.5, 0.5);
+		double[][] v = initMatrix(inputSize+1, instSize, 0.25);
 		// for(int r=0;r<v.length;r++){
 		// 	for(int c=0;c<v[r].length;c++){
 		// 		if(c%2==0) v[r][c] = -0.5;
 		// 		v[r][c] = My.stepify(v[r][c], acc);
 		// 	}
 		// }
-		double[][] w = initMatrix(instSize+1, outputSize, -0.5, 0.5);
+		double[][] w = initMatrix(instSize+1, outputSize, 0.45);
 		// for(int r=0;r<w.length;r++){
 		// 	for(int c=0;c<w[r].length;c++){
 		// 		if(c%2==0) w[r][c] = 0.5;
